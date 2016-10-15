ad7f77bb4fc66e440f7e4985fde7c8b2d8d3406f
Buschmann <kontakt@buschmann23.de>
2016-10-15 15:08:47 +0200

    Fix shared export.

--- src/fuoten_global.h
+++ src/fuoten_global.h
@@ -1,12 +1,34 @@
+/* libfuoten - Qt based library to access the ownCloud/Nextcloud News App API
+ * Copyright (C) 2016 Buschtrommel / Matthias Fehring
+ * https://www.buschmann23.de/entwicklung/bibliotheken/libfuoten/
+ * https://github.com/Buschtrommel/libfuoten
+ *
+ * fuoten_global.h
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef FUOTEN_GLOBAL_H
 #define FUOTEN_GLOBAL_H
 
 #include <QtCore/qglobal.h>
 
 #if defined(FUOTEN_LIBRARY)
-#  define GELTANSHARED_EXPORT Q_DECL_EXPORT
+#  define FUOTENSHARED_EXPORT Q_DECL_EXPORT
 #else
-#  define GELTANSHARED_EXPORT Q_DECL_IMPORT
+#  define FUOTENSHARED_EXPORT Q_DECL_IMPORT
 #endif
 
 #endif // FUOTEN_GLOBAL_H
