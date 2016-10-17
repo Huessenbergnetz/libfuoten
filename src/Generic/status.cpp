6fc6861971f26ac5750d758ad637b354fa910fb8
Buschmann <kontakt@buschmann23.de>
2016-10-17 11:49:45 +0200

    Implement status route class to request News App status.

---
+++ src/Generic/status.cpp
@@ -0,0 +1,107 @@
+/* libfuoten - Qt based library to access the ownCloud/Nextcloud News App API
+ * Copyright (C) 2016 Buschtrommel / Matthias Fehring
+ * https://www.buschmann23.de/entwicklung/bibliotheken/libfuoten/
+ * https://github.com/Buschtrommel/libfuoten
+ *
+ * generic/status.cpp
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "status_p.h"
+#include "../error.h"
+#include <QJsonValue>
+
+
+using namespace Fuoten;
+using namespace Generic;
+
+Status::Status(QObject *parent) :
+    Component(* new StatusPrivate, parent)
+{
+    setApiRoute(QStringLiteral("/status"));
+    setExpectedJSONType(Component::Object);
+}
+
+Status::Status(StatusPrivate &dd, QObject *parent) :
+    Component(dd, parent)
+{
+    setApiRoute(QStringLiteral("/status"));
+    setExpectedJSONType(Component::Object);
+}
+
+
+void Status::get()
+{
+    if (inOperation()) {
+        return;
+    }
+
+    setInOperation(true);
+
+    sendRequest();
+}
+
+
+void Status::successCallback()
+{
+    Q_D(const Status);
+    if (configuration()) {
+        configuration()->setServerVersion(d->resultObject.value(QStringLiteral("version")).toString());
+        const QJsonObject w = d->resultObject.value(QStringLiteral("warnings")).toObject();
+        if (!w.isEmpty()) {
+            configuration()->setImproperlyConfiguredCron(w.value(QStringLiteral("improperlyConfiguredCron")).toBool());
+        }
+    }
+    setInOperation(false);
+    Q_EMIT succeeded(jsonResult());
+}
+
+
+void Status::extractError(QNetworkReply *reply)
+{
+    setError(new Error(reply, this));
+    setInOperation(false);
+    Q_EMIT failed(error());
+}
+
+
+
+bool Status::checkOutput()
+{
+    if (Component::checkOutput()) {
+
+        Q_D(Status);
+
+        d->resultObject = jsonResult().object();
+
+        if (!d->resultObject.contains(QStringLiteral("version"))) {
+            setError(new Error(Error::OutputError, Error::Critical, tr("Can not find the version information in the server reply."), QString(), this));
+            Q_EMIT failed(error());
+            return false;
+        }
+
+        if (!d->resultObject.contains(QStringLiteral("warnings"))) {
+            setError(new Error(Error::OutputError, Error::Critical, tr("Can not find the warnings information in the server reply."), QString(), this));
+            Q_EMIT failed(error());
+            return false;
+        }
+
+    } else {
+        return false;
+    }
+
+    return true;
+}
---
+++ src/Generic/status.h
@@ -0,0 +1,78 @@
+/* libfuoten - Qt based library to access the ownCloud/Nextcloud News App API
+ * Copyright (C) 2016 Buschtrommel / Matthias Fehring
+ * https://www.buschmann23.de/entwicklung/bibliotheken/libfuoten/
+ * https://github.com/Buschtrommel/libfuoten
+ *
+ * generic/status.h
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef STATUS_H
+#define STATUS_H
+
+#include <QObject>
+#include "../component.h"
+#include "../fuoten_global.h"
+
+namespace Fuoten {
+namespace Generic {
+
+class StatusPrivate;
+
+/*!
+ * \brief Requests the status from the News App.
+ *
+ * The status reply will contain the News App version number an possible warnings about improperly configurations.
+ * To request the status, set the \link Component::configuration configuration \endlink property and call get().
+ *
+ * The requested data will be written to Configuration::setServerVersion() and Configuration::setImproperlyConfiguredCron(). You can get the raw JSON response from the Component::succeeded() signal.
+ *
+ * \sa AccountValidator
+ * \headerfile "" <Fuoten/Generic/status.h>
+ */
+class FUOTENSHARED_EXPORT Status : public Component
+{
+    Q_OBJECT
+public:
+    /*!
+     * \brief Constructs a new Status object.
+     */
+    Status(QObject *parent = nullptr);
+
+    /*!
+     * \brief Starts the API request.
+     */
+    Q_INVOKABLE void get();
+
+protected:
+    Status(StatusPrivate &dd, QObject *parent = nullptr);
+
+    void successCallback() Q_DECL_OVERRIDE;
+
+    void extractError(QNetworkReply *reply) Q_DECL_OVERRIDE;
+
+    bool checkOutput() Q_DECL_OVERRIDE;
+
+private:
+    Q_DECLARE_PRIVATE(Status)
+    Q_DISABLE_COPY(Status)
+
+};
+
+}
+}
+
+#endif // STATUS_H
