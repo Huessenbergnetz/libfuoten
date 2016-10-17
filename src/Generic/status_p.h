d76463cd68c889f365a8ddedc2b6c8622391493c
Buschmann <kontakt@buschmann23.de>
2016-10-17 11:50:43 +0200

    Damn! Forgot private headers.

---
+++ src/Generic/status_p.h
@@ -0,0 +1,44 @@
+/* libfuoten - Qt based library to access the ownCloud/Nextcloud News App API
+ * Copyright (C) 2016 Buschtrommel / Matthias Fehring
+ * https://www.buschmann23.de/entwicklung/bibliotheken/libfuoten/
+ * https://github.com/Buschtrommel/libfuoten
+ *
+ * generic/status_p.h
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef STATUS_P
+#define STATUS_P
+
+#include "status.h"
+#include "../component_p.h"
+#include <QJsonObject>
+
+namespace Fuoten {
+namespace Generic {
+
+class StatusPrivate : public ComponentPrivate {
+public:
+    StatusPrivate() : ComponentPrivate() {}
+
+    QJsonObject resultObject;
+};
+
+}
+}
+
+#endif // STATUS_P
+
---
+++ src/Generic/version_p.h
@@ -0,0 +1,45 @@
+/* libfuoten - Qt based library to access the ownCloud/Nextcloud News App API
+ * Copyright (C) 2016 Buschtrommel / Matthias Fehring
+ * https://www.buschmann23.de/entwicklung/bibliotheken/libfuoten/
+ * https://github.com/Buschtrommel/libfuoten
+ *
+ * generic/version_p.h
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VERSION_P
+#define VERSION_P
+
+#include "version.h"
+#include "../component_p.h"
+#include <QJsonObject>
+
+namespace Fuoten {
+namespace Generic {
+
+class VersionPrivate : public ComponentPrivate {
+public:
+    VersionPrivate() : ComponentPrivate() {}
+
+    QJsonObject resultObject;
+};
+
+}
+
+}
+
+#endif // VERSION_P
+
