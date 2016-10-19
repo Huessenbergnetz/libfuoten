c8d9f6535df2ca07f07d58319c96690df84a99dd
Buschmann <kontakt@buschmann23.de>
2016-10-20 01:39:21 +0200

    VersionNumber: add license header

--- src/Helpers/versionnumber.cpp
+++ src/Helpers/versionnumber.cpp
@@ -1,3 +1,25 @@
+/* libfuoten - Qt based library to access the ownCloud/Nextcloud News App API
+ * Copyright (C) 2016 Buschtrommel / Matthias Fehring
+ * https://www.buschmann23.de/entwicklung/bibliotheken/libfuoten/
+ * https://github.com/Buschtrommel/libfuoten
+ *
+ * helpers/versionnumber.cpp
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #include "versionnumber.h"
 #include <QRegularExpression>
 #include <QRegularExpressionMatch>
