c7a41af88e8e4451bc26d84c2e615792abca73ec
Buschmann <kontakt@buschmann23.de>
2017-10-26 14:51:38 +0200

    Add release script

---
+++ scripts/makeRelease.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+CURRENTDIR=$PWD
+TMPDIR=/tmp
+PRJNAME=libfuoten
+SIGNKEY=3A70A936614C3258
+
+if [ "$1" == "" ]; then
+	echo "You have to specify a version number"
+	exit 1
+fi
+
+VERSION=$1
+
+if [ "$2" == "" ]; then
+	echo "You have to specify an output directory"
+	exit 1
+fi
+
+OUTDIR=$2
+
+TARNAME="${PRJNAME}-${VERSION}"
+
+DIR="${TMPDIR}/${TARNAME}"
+
+rm -rf $DIR
+
+./scripts/releasel10n.sh
+
+mkdir -p $DIR
+
+cp -a -r -t $DIR *
+
+rm "$DIR/scripts/doxyWebFooter.html"
+rm "$DIR/scripts/makeWebDocs.sh"
+
+pushd "${TMPDIR}" > /dev/null
+
+rm -f "${OUTDIR}/${TARNAME}.tar.xz"
+rm -f "${OUTDIR}/${TARNAME}.tar.gz"
+
+tar -cJf "${OUTDIR}/${TARNAME}.tar.xz" ${TARNAME}
+tar -czf "${OUTDIR}/${TARNAME}.tar.gz" ${TARNAME}
+
+rm -rf ${TARNAME}
+
+popd > /dev/null
+
+gpg --armor --detach-sign --yes --default-key $SIGNKEY "${OUTDIR}/${TARNAME}.tar.xz"
+gpg --armor --detach-sign --yes --default-key $SIGNKEY "${OUTDIR}/${TARNAME}.tar.gz"
+
+exit 0
