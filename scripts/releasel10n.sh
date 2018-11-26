76f066a9af1d81c056e25fa8c3a5d4e88f87ad51
Buschmann <mf@huessenbergnetz.de>
2018-11-26 22:59:47 +0100

    Improve scripts to create translations

--- scripts/releasel10n.sh
+++ scripts/releasel10n.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+if [ $(which lrelease-qt5) ]; then
+        LRELEASE=`which lrelease-qt5`
+        echo "Found lrelease at $LRELEASE"
+fi
+
+if [ ! -x $LUPDATE ]; then
+        if [ $(which lrelease) ]; then
+                LRELEASE=`which lrelease`
+                echo "Found lrelease at $LRELEASE"
+        else
+                echo "Can not find executable for lrelease!"
+                exit 1
+        fi
+fi
+
+if [ ! -d translations ]; then
+	echo "Directory translations not available!"
+	echo "Maybe you have to run scripts/updatesl10n.sh at first."
+	echo 1
+fi
+
 lrelease-qt5 -idbased translations/libfuoten.ts
 
 for LANG in da de en_US en_GB fr nl sv
--- scripts/updatel10n.sh
+++ scripts/updatel10n.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-if [ ! -d translations ]; then
-    mkdir translations
+if [ $(which lupdate-qt5) ]; then
+	LUPDATE=`which lupdate-qt5`
+	echo "Found lupdate at $LUPDATE"
 fi
 
-lupdate-qt5 -no-obsolete -pluralonly -source-language en -target-language en_GB -locations none Fuoten -ts translations/libfuoten_en_GB.ts
-lupdate-qt5 -no-obsolete -pluralonly -source-language en -target-language en_US -locations none Fuoten -ts translations/libfuoten_en_US.ts
-lupdate-qt5 -no-obsolete -source-language en -target-language en -locations none Fuoten -ts translations/libfuoten.ts
+if [ ! -x $LUPDATE ]; then
+	if [ $(which lupdate) ]; then
+		LUPDATE=`which lupdate`
+		echo "Found lupdate at $LUPDATE"
+	else
+		echo "Can not find executable for lupdate!"
+		exit 1
+	fi
+fi
+
+if [ ! -d translations ]; then
+    mkdir translations
+    if [ ! -d translations ]; then
+	echo "Failed to create translations directory!"
+	exit 1
+    fi
+fi
+
+$LUPDATE -no-obsolete -pluralonly -source-language en -target-language en_GB -locations none Fuoten -ts translations/libfuoten_en_GB.ts
+$LUPDATE -no-obsolete -pluralonly -source-language en -target-language en_US -locations none Fuoten -ts translations/libfuoten_en_US.ts
+$LUPDATE -no-obsolete -source-language en -target-language en -locations none Fuoten -ts translations/libfuoten.ts
