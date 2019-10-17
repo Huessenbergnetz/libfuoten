33ba43bab728872e1d52477efe3dff5e9c12c243
Buschmann <mf@huessenbergnetz.de>
2019-10-17 12:52:39 +0200

    Improve translation extration and release scripts

--- scripts/releasel10n.sh
+++ scripts/releasel10n.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-if [ $(which lrelease-qt5) ]; then
-        LRELEASE=`which lrelease-qt5`
-        echo "Found lrelease at $LRELEASE"
+LRELEASE=`which lrelease-qt5`
+if [ ! -x $LRELEASE ]
+then
+    LRELEASE=`which lrelease`
 fi
 
-if [ ! -x $LUPDATE ]; then
-        if [ $(which lrelease) ]; then
-                LRELEASE=`which lrelease`
-                echo "Found lrelease at $LRELEASE"
-        else
-                echo "Can not find executable for lrelease!"
-                exit 1
-        fi
+if [ -x $LRELEASE ]
+then
+    echo "Found lrelease at $LRELEASE"
+else
+    echo "Can neither find lrelease nor lrelease-qt5"
+    exit 1
 fi
 
 if [ ! -d translations ]; then
-	echo "Directory translations not available!"
-	echo "Maybe you have to run scripts/updatesl10n.sh at first."
-	echo 1
+    echo "Directory translations not available!"
+    echo "Maybe you have to run scripts/updatel10n.sh at first."
+    echo 1
 fi
 
 $LRELEASE -idbased translations/libfuoten.ts
--- scripts/updatel10n.sh
+++ scripts/updatel10n.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-if [ $(which lupdate-qt5) ]; then
-	LUPDATE=`which lupdate-qt5`
-	echo "Found lupdate at $LUPDATE"
+LUPDATE=`which lupdate-qt5`
+if [ ! -x $LUPDATE ]
+then
+    LUPDATE=`which lupdate`
 fi
 
-if [ ! -x $LUPDATE ]; then
-	if [ $(which lupdate) ]; then
-		LUPDATE=`which lupdate`
-		echo "Found lupdate at $LUPDATE"
-	else
-		echo "Can not find executable for lupdate!"
-		exit 1
-	fi
+if [ -x $LUPDATE ]
+then
+    echo "Found lupdate at $LUPDATE"
+else
+    echo "Can neither find lupdate-qt nor lupdate"
+    exit 1
 fi
 
 if [ ! -d translations ]; then
     mkdir translations
     if [ ! -d translations ]; then
-	echo "Failed to create translations directory!"
-	exit 1
+        echo "Failed to create translations directory!"
+        exit 1
     fi
 fi
 
