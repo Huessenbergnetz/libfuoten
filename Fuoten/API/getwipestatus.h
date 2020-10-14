0d8289fcb3f56722c2baccc0919bbce2f9a3a4a5
Buschmann <mf@huessenbergnetz.de>
2020-10-14 21:10:43 +0200

    Add API docs to wipe classes (#60)

--- Fuoten/API/getwipestatus.h
+++ Fuoten/API/getwipestatus.h
@@ -28,6 +28,32 @@ namespace Fuoten {
 
 class GetWipeStatusPrivate;
 
+/*!
+ * \brief Requests the wipe status from the Nextcloud.
+ *
+ * This will get the wipe status form the Nextcloud configured in the \link Component::configuration configuration\endlink
+ * object used by this object. So that property is mandatory. This should be used after you get a server reply
+ * with HTTP status code 401 or 403. If a wipe is requested, the wipeRequested() signal will be emitted. After
+ * the application data has been wiped on the local device, you should use PostWipeSuccess to inform the remote
+ * Nextcloud that the wipe has been executed. There is already WipeManager that can handle remote wipe requests.
+ *
+ * See also https://docs.nextcloud.com/server/latest/developer_manual/client_apis/RemoteWipe/index.html
+ *
+ * \par Mandatory properties
+ * Component::configuration
+ *
+ * \par API route
+ * Not part of the News App API, uses general Nextcloud API at /index.php/core/wipe/check
+ *
+ * \par Method
+ * GET
+ *
+ * \since 0.8.0
+ *
+ * \sa WipeManager, PostWipeSuccess
+ *
+ * \headerfile "" <Fuoten/API/GetWipeStatus>
+ */
 class FUOTENSHARED_EXPORT GetWipeStatus : public Component
 {
     Q_OBJECT
@@ -42,16 +68,40 @@ public:
      */
     ~GetWipeStatus() override;
 
+    /*!
+     * \brief Executes the API request.
+     *
+     * To perform a successful API request, Component::configuration has to be set to a valid object
+     * derived from AbstractConfiguration.
+     *
+     * Execution will not run if the \link Component::inOperation inOperation\endlink property returns
+     * \c true and will itself set that property to \c true when starting the request.
+     */
     Q_INVOKABLE void execute() override;
 
 Q_SIGNALS:
+    /*!
+     * \brief Triggered when a remote wipe has been requested.
+     *
+     * This signal will be emitted in the successCallback().
+     */
     void wipeRequested();
 
 protected:
     GetWipeStatus(GetWipeStatusPrivate &dd, QObject *parent = nullptr);
 
+    /*!
+     * \brief Checks the input parameters.
+     *
+     * Will especially check if the password has been set.
+     */
     bool checkInput() override;
 
+    /*!
+     * \brief Will check the reply if a remote wipe is requested.
+     *
+     * If a wipe has been requested, the wipeRequested() signal will be emitted.
+     */
     void successCallback() override;
 
 private:
--- Fuoten/API/postwipesuccess.h
+++ Fuoten/API/postwipesuccess.h
@@ -28,6 +28,31 @@ namespace Fuoten {
 
 class PostWipeSuccessPrivate;
 
+/*!
+ * \brief Signals wipe completion to the Nextcloud server.
+ *
+ * This posts the success message to the Nextcloud server after a requested remote wipe
+ * has been successfuly performed on the local device. Should be used after a remote wipe
+ * has been signaled by GetWipeStatus::wipeRequested() and the local wipe has been performed.
+ * There is already WipeManager that can handle remote wipe requests.
+ *
+ * See also https://docs.nextcloud.com/server/latest/developer_manual/client_apis/RemoteWipe/index.html
+ *
+ * \par Mandatory properties
+ * Component::configuration
+ *
+ * \par API route
+ * Not part of the News App API, uses general Nextcloud API at /index.php/core/wipe/success
+ *
+ * \par Method
+ * POST
+ *
+ * \since 0.8.0
+ *
+ * \sa WipeManager, GetWipeStatus
+ *
+ * \headerfile "" <Fuoten/API/PostWipeSuccess>
+ */
 class FUOTENSHARED_EXPORT PostWipeSuccess : public Component
 {
     Q_OBJECT
@@ -42,13 +67,30 @@ public:
      */
     ~PostWipeSuccess() override;
 
+    /*!
+     * \brief Executes the API request.
+     *
+     * To perform a successful API request, the \link Component::configuration configuration\endlink
+     * property has to be set to a valid object derived from AbstractConfiguration.
+     *
+     * Execution will not run if the \link Component::inOperation inOperation\endlink property returns
+     * \c true and will itself set that property to \c true when starting the request.
+     */
     Q_INVOKABLE void execute() override;
 
 protected:
     PostWipeSuccess(PostWipeSuccessPrivate &dd, QObject *parent = nullptr);
 
+    /*!
+     * \brief Checks the input parameters.
+     *
+     * Will especially check if the password has been set.
+     */
     bool checkInput() override;
 
+    /*!
+     * \brief Will emit the succeeded() signal.
+     */
     void successCallback() override;
 
 private:
